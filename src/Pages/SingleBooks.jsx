ceb1d5cc78ee10270bb5bb79a9f7f6f57110a37b
RupeshSahu <75201337+RupeshSahu969@users.noreply.github.com>
2023-05-24 17:55:56 +0530

    add new data

--- src/Pages/SingleBooks.jsx
+++ src/Pages/SingleBooks.jsx
@@ -1,12 +1,26 @@
-import React from 'react'
-import { useParams } from 'react-router-dom'
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { useNavigate, useParams } from 'react-router-dom'
+import { getBooks} from '../Redux/AppReducer/actionCreator'
 
 const SingleBooks = () => {
-  const id=useParams()
+  const books=useSelector((state)=>state.AppReducer.books);
+
+  const {id}=useParams()
   console.log(id)
+  const navigate=useNavigate()
+  const dispatch = useDispatch();
+  useEffect(() => {
+
+    dispatch(getBooks(id));
+
+  }, [id]);
+  
+  
   return (
     <div>
       <h1>SingleBook Page</h1>
+      <div> {books.id} </div>
     </div>
   )
 }
